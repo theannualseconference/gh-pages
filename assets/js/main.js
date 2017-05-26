49d7d849306d3bd8e1d82532b47f4f28d097d670
Forestry.io <support@forestry.io>
2017-05-26 14:13:35 +0000

    Deploy from Forestry.io

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,47 @@
-console.log('hello world')
-/* globals $ */
+console.log('hello world');
+/* globals jQuery $ scrollMonitor */
+
+
+// Monkey patching jquery scroll event
+(function ($) {
+  var wnd = $(window);
+  var lastScrollTop = wnd.scrollTop();
+  var lastScrollLeft = wnd.scrollLeft();
+
+  wnd.on("scroll", function (event) {
+      var scrollTop = wnd.scrollTop();
+      var scrollLeft = wnd.scrollLeft();
+
+      event.direction = scrollTop > lastScrollTop ? 'down' :
+        scrollTop < lastScrollTop ? 'up' : 
+        scrollLeft > lastScrollLeft ? 'right' : 
+        scrollLeft < lastScrollLeft ? 'left' : 
+        undefined;
+
+
+      lastScrollTop = scrollTop;
+      lastScrollLeft = scrollLeft;
+    });
+}(jQuery));
+
+
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      fn.apply(fn ,arguments);
+      time = Date.now();
+    }
+  }
+}
+
+
 
 function loadTab(el) {
   let $el = $(el);
   let tabId = $el.attr('data-tab');
   $el
-    .parents('section')
+    .parents('section, article')
     .find('.active')
     .removeClass('active');
   
@@ -18,9 +54,9 @@ function loadTab(el) {
 }
 
 function scrollTo(hash) {
-  $('html, body').animate({
-    scrollTop: $(`#${hash}`).offset().top
-  }, 0);
+  // $('html, body').animate({
+  //   scrollTop: $(`#${hash}`).offset().top
+  // }, 0);
 }
 
 $(function() {  
@@ -39,5 +75,102 @@ $(function() {
     let id = loadTab(e.currentTarget);
     window.location.hash = '/' + id;
   })
+
+
+  /* Setting up the modal */
+  $('.modal').click((e) => {
+    if (
+      $('.modal').is(event.target) || 
+      $('.close').is(event.target)
+    ) $('.modal').css('display', 'none');
+  })
+
+  $('.registration-link__open').click(() => {
+    $('.modal').css('display', 'block');
+  })
+
+
+
+// transform:translateY(-600px);
+
+  /* Scroll stuff */
+
+  $(window).scroll(throttle(function(e) {
+    // console.log(e.direction);
+
+
+
+
+  }, 100))
+
+  // $('.tab-titles__speaker').scrollToFixed();
+
+  $('.tab-titles__speaker').toArray().forEach(el => {
+    var $el = $(el);
+    var bottomMargin = $el.height() + 22;
+    var $nextPanel = $el.parents('.panel').next();
+
+    $el.scrollToFixed({ 
+      marginTop: 22, 
+      // postFixed() { $(this).addClass('top-2'); },
+
+      limit: () => $nextPanel.offset().top - bottomMargin
+    });
+
+
+  })
+
+
+
+
+  $('.cover__masonry').masonry({
+    // options
+    itemSelector: '.brick',
+    columnWidth: '.brick',
+    percentPosition: true
+  });
+
+  // $('.cover__masonry').hide();
+
+  // var speakerMonitors = $('.section-flex-container__speaker').parents('.panel').toArray().map(el => scrollMonitor.create(el));
+
+  // speakerMonitors.forEach(elw => {
+
+  //   elw.enterViewport(function() {
+  //     $(elw.watchItem).find('.tab-titles__speaker')
+  //       .addClass('entering')
+
+  //     console.log('Entering')
+  //   })
+
+  //   elw.fullyEnterViewport(function() {
+  //     $(elw.watchItem).find('.tab-titles__speaker')
+  //       .removeClass('entering')
+  //       .addClass('fixed top-2')
+
+  //     console.log('Entered');
+  //   });
+
+  //   elw.partiallyExitViewport(function() {
+  //     $(elw.watchItem).find('.tab-titles__speaker')
+  //       .addClass('leaving')
+
+  //     // elw.on('scroll', function(ee) {
+  //     //   console.log('wowza', ee)
+  //     // })
+
+  //     console.log('Leaving');
+  //   });
+
+  //     elw.exitViewport(function() {
+  //     $(elw.watchItem).find('.tab-titles__speaker')
+  //       .removeClass('fixed')
+
+  //     console.log('Left');
+  //   });
+  // });
+
+
+
 
 })
